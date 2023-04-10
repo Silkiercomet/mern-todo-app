3d071099d1eace8829a78e2a419d640fddaf5b24
luis Colina <colina.1990lz@gmail.com>
2023-04-10 08:59:35 -0400

    logic of get, patch and post routes made

--- backend/index.js
+++ backend/index.js
@@ -4,24 +4,20 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 const app = express();
 const connectDB = require("./db/connect");
-const todoRoutes = require("./routes/router")
+const todoRoutes = require("./routes/router");
 
 app.use(bodyParser.json());
 app.use(cors());
 dotenv.config({ path: "./config.env" });
 
-app.use("/api", todoRoutes)
-
+app.use("/api", todoRoutes);
 
 const start = async () => {
   try {
     // this way is gonna connect to mongodb before running the server
     await connectDB(process.env.MONGO_URI);
-
     app.listen(process.env.PORT, () => {
-      console.log(
-        `server is connected at ${process.env.PORT}`
-      );
+      console.log(`server is connected at ${process.env.PORT}`);
     });
   } catch (err) {
     console.log(err);
